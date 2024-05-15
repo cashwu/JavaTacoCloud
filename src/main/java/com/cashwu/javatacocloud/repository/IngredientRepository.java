0a68dc0f8b4789ab0e5dd82c4bb2fbcac695fa78
Cash <cashwugeek@icloud.com>
2024-05-15 15:02:19 +0800

    ch3 spring data jdbc

--- src/main/java/com/cashwu/javatacocloud/repository/IngredientRepository.java
+++ src/main/java/com/cashwu/javatacocloud/repository/IngredientRepository.java
@@ -1,6 +1,7 @@
 package com.cashwu.javatacocloud.repository;
 
 import com.cashwu.javatacocloud.model.Ingredient;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,13 +10,13 @@ import java.util.Optional;
  * @author cash.wu
  * @since 2024/05/15
  */
-public interface IngredientRepository {
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
-    Iterable<Ingredient> findAll();
-
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
+//    Iterable<Ingredient> findAll();
+//
+//    Optional<Ingredient> findById(String id);
+//
+//    Ingredient save(Ingredient ingredient);
 }
 
 
--- src/main/java/com/cashwu/javatacocloud/repository/OrderRepository.java
+++ src/main/java/com/cashwu/javatacocloud/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.cashwu.javatacocloud.repository;
 
 import com.cashwu.javatacocloud.model.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
@@ -8,9 +9,9 @@ import java.util.Optional;
  * @author cash.wu
  * @since 2024/05/15
  */
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
-    TacoOrder save(TacoOrder tacoOrder);
-
-    Optional<TacoOrder> findById(Long id);
+//    TacoOrder save(TacoOrder tacoOrder);
+//
+//    Optional<TacoOrder> findById(Long id);
 }
