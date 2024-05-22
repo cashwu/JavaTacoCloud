3186b606ee50334cfe415093d66b15ebc853d276
Cash <cashwugeek@icloud.com>
2024-05-22 14:02:39 +0800

    ch7 rest data

--- src/main/java/com/cashwu/javatacocloud/model/Taco.java
+++ src/main/java/com/cashwu/javatacocloud/model/Taco.java
@@ -7,9 +7,11 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @Entity
 @Data
+@RestResource(rel = "tacos", path = "tacos")
 public class Taco {
 
   @Id
